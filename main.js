2fca63c476bbdfc170e6bc8a9639aa34b6b86020
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-05-12 14:01:49 +0900

    startButton_implement

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ const init = () => {
         const element = document.createElement('div')
         container.appendChild(element)
         // この辺の宣言をする時，頭の中で，もういじることがなかったら，const にできてる感じが良かったと思う
+        // この辺，一つずつ宣言するんじゃなくって，
         const buttonSize = size * 0.25
         const radius = size * 0.35
         const center = size * 0.5
@@ -43,11 +44,40 @@ const init = () => {
         // buttonSize/2 を入れないと右下に偏る（当たり前）
         element.style.top = `${center + radius * Math.sin(angle) - buttonSize / 2}px`
         element.style.left = `${center + radius * Math.cos(angle) - buttonSize / 2}px`
-        element.style.backgroundColor = `hsl(${i * 360 / buttonNumber}, 100%, 50%)`
+        element.style.backgroundColor = `hsl(${i * 360 / buttonNumber}, 100%, 20%)`
         // （何で 50% で円になるの？という感じはある）
         element.style.borderRadius = '50%'
+        // いい感じの円環ができた，これが回ると考えるとちょっとワクワクしちゃうね
     }
 
+    // ここで，スタートボタンのコンテナまでも用意する必要はあるんだろうか，と思った
+    // タッチ判定とかそういう話？でも別にコンテナを分ける必要はなないと思うけど
+    const startButtonContainer = document.createElement('div')
+    document.body.appendChild(startButtonContainer)
+    startButtonContainer.style.position = `absolute`
+    startButtonContainer.style.width = `${size}px`
+    startButtonContainer.style.height = `${size}px`
+    startButtonContainer.style.display = `flex`
+    startButtonContainer.style.justifyContent = `center`
+    startButtonContainer.style.alignItems = `center`
+    // 勝手に真ん中寄せできるのはまあ楽と言えば楽か・・・？
+
+    const startButton = document.createElement('div')
+    startButtonContainer.appendChild(startButton)
+    startButton.style.backgroundColor = '#00f'
+    startButton.style.width = `${size/3}px`
+    startButton.style.height = `${size/6}px`
+    startButton.style.display = `flex`
+    startButton.style.justifyContent = `center`
+    startButton.style.alignItems = `center`
+    // これ，% 指定だとなんか形がキモくなる（なんで？）
+    startButton.style.borderRadius = `${size/20}px`
+    startButton.style.color = '#fff'
+    startButton.style.fontFamily = 'Times New Roman'
+    startButton.style.fontSize = `${size/12}px`
+    startButton.textContent = 'Start'
+    // 見栄えを調整するのに結構時間がかかってて，ここを手早く済ませる方法はないだろうか
+    // それこそ react か？
 
 
     document.onpointerdown = async (e) => {
